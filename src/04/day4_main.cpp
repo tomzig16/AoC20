9c4cd63c502a8225e5b7ba59c1c02b38ef25c14a
tomzig16 <tzigmantavicius@gmail.com>
2020-12-04 21:37:20 +0200

    Solve day 4

--- src/04/day4_main.cpp
+++ src/04/day4_main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
+#include <regex>
 #include <string>
 #include <map>
 #include "../HelperFunctions.h"
@@ -14,6 +14,52 @@ struct PassportData {
 	bool IsValidPassport() {
 		return (data.size() == 8) ||
 			((data.size() == 7) && (data.find("cid") == data.end()));
+	}
+
+	// byr, iyr and eyr have the same requirements but different min max
+	bool YearIsValid(std::string key, int min, int max) {
+		std::string val = data[key];
+		if (val.size() != 4) return false;
+		int number = StringToInt(val);
+		return number >= min && number <= max;
+	}
+
+	bool HgtIsValid() {
+		std::string val = data["hgt"];
+		std::string units = val.substr(val.size() - 2, 2);
+		val.erase(val.size() - 2, val.size());
+		int number = StringToInt(val);
+		if (units == "cm") {
+			return number >= 150 && number <= 193;
+		}
+		else if (units == "in") {
+			return number >= 59 && number <= 76;
+		}
+		return false;
+	}
+
+	bool HclIsValid() {
+		std::string val = data["hcl"];
+		std::regex regexForColor("^#[a-fA-F0-9]{6}$");
+		return std::regex_search(val, regexForColor);
+	}
+
+	bool EclIsValid() {
+		std::string val = data["ecl"];
+		return
+			val == "amb" ||
+			val == "blu" ||
+			val == "brn" ||
+			val == "gry" ||
+			val == "grn" ||
+			val == "hzl" ||
+			val == "oth";
+	}
+
+	bool PidIsValid() {
+		std::string val = data["pid"];
+		std::regex regexForColor("^\d{9}$");
+		return std::regex_search(val, regexForColor);
 	}
 };
 
@@ -68,14 +114,26 @@ int FirstPart(std::vector<PassportData> passportData) {
 	return counter;
 }
 
-int SecondPart(std::vector<std::string> inputs) {
-	return -1;
+int SecondPart(std::vector<PassportData> passportData) {
+	int counter = 0;
+	for (PassportData pd : passportData) {
+		if (!pd.IsValidPassport()) continue;
+		if(!pd.YearIsValid("byr", 1920, 2002)) continue;
+		if(!pd.YearIsValid("iyr", 2010, 2020)) continue;
+		if(!pd.YearIsValid("eyr", 2020, 2030)) continue;
+		if(!pd.HgtIsValid()) continue;
+		if(!pd.HclIsValid()) continue;
+		if(!pd.EclIsValid()) continue;
+		if(!pd.PidIsValid()) continue;
+		counter++;
+	}
+	return counter;
 }
 
 int main() {
 	std::vector<std::string> inputs = ReadStrings("input.txt");
 	std::vector<PassportData> pd = GeneratePassportData(inputs);
 	PRINT(1, FirstPart(pd));
-	PRINT(2, SecondPart(inputs));
+	PRINT(2, SecondPart(pd));
 	return 0;
 }
